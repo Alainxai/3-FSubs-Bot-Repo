0eba995499e8a041a52df633c089819b62c85868
ATOMIC-00 <132604794+ATOMIC-00@users.noreply.github.com>
2024-02-02 01:16:51 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -11,7 +11,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>┏━━━━━•◦●◉✿ ❟❛❟ ✿◉●◦•━━━━━━┓\n× ɢᴏᴅ : <a href='https://t.me/Lucifer_x0o'>ᴀʙʀᴀʜᴀᴍ™</a>\n× ᴅᴇᴍɪ-ɢᴏᴅ : <a href='tg://user?id=6193451722'>chotta.||..Shivam</a>\n× ʜᴇɴᴛᴀɪ ᴄʜᴀɴɴᴇʟ : <a href='https://t.me/Hentaii_flix'>ʜᴇɴᴛᴀɪ ꜰʟɪx</a>\n× ᴀɴɪᴍᴇ ᴄʜᴀɴɴᴇʟ 1 : <a href='https://t.me/animation_hub_b'>ᴀɴɪᴍᴀᴛɪᴏɴ ʜᴜʙ</a>\n× ᴀɴɪᴍᴇ ᴄʜᴀɴɴᴇʟ 2 : <a href='https://t.me/Anime_Yugen'>ᴀɴɪᴍᴇ ʏᴜɢᴇɴ</a>\n× ᴍᴀɴɢᴀ ᴄʜᴀɴɴᴇʟ : <a href='https://t.me/Manga_Yugen'>ᴍᴀɴɢᴀ ʏᴜɢᴇɴ</a>\n┗━━━━━•◦●◉✿ ❟❛❟ ✿◉●◦•━━━━━━┛</b>",
+            text = f"<b>┏━━━━━•◦●◉✿ ❟❛❟ ✿◉●◦•━━━━━━┓\n× ɢᴏᴅ : <a href='https://t.me/Lucifer_x0o'>ᴀʙʀᴀʜᴀᴍ™</a>\n× ᴅᴇᴍɪ-ɢᴏᴅ : <a href='tg://user?id=6193451722'>chotta.||..Shivam</a>\n× ʜᴇɴᴛᴀɪ ᴄʜᴀɴɴᴇʟ : <a href='https://t.me/Hentaii_flix'>ʜᴇɴᴛᴀɪ ꜰʟɪx</a>\n× ʙᴏᴛ ꜰᴏʀ : <a href='https://t.me/Anime_Flix_Network'>ᴀɴɪᴍᴇ ꜰʟɪx ɴᴇᴛᴡᴏʀᴋ</a>\n× ᴀɴɪᴍᴇ ᴄʜᴀɴɴᴇʟ 2 : <a href='https://t.me/Anime_Yugen'>ᴀɴɪᴍᴇ ʏᴜɢᴇɴ</a>\n× ᴍᴀɴɢᴀ ᴄʜᴀɴɴᴇʟ : <a href='https://t.me/Manga_Yugen'>ᴍᴀɴɢᴀ ʏᴜɢᴇɴ</a>\n┗━━━━━•◦●◉✿ ❟❛❟ ✿◉●◦•━━━━━━┛</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
